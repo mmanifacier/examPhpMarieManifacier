2455530ee6662ff3e4fdca74bbe7647e071a9a83
mmanifacier <marie.manifacier@hotmail.fr>
2021-06-01 15:45:05 +0200

    delete carousel

--- add-player.php
+++ add-player.php
@@ -12,7 +12,6 @@
         session_start();
         require './function/function.php';
         checkConnexion();
-        include './parts/carousel.php';
         include './parts/navbar.php';
     ?>
     <div class="container">
